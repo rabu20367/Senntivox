acf783b533da3e7fa9b20736cf7b7a3f52a4ed1b
hasibur_rashid <atm.hasibur.rashid20367@gmail.com>
2025-06-09 19:11:03 -0400

    Update App test to check home page hero text

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders home page hero text', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heroText = screen.getByText(/your intelligent emotional well-being companion/i);
+  expect(heroText).toBeInTheDocument();
 });
